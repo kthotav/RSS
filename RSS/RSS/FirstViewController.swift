b5bb120fe82fecba7cf8ecc8c17f3c50634f4582
Karthik Thota <kthotav@gmail.com>
2019-02-03 16:35:30 -0800

    Initial Commit
    - Added Travis CI

---
+++ RSS/RSS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RSS
+//
+//  Created by Karthik Personal on 2/3/19.
+//  Copyright © 2019 VKT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RSS/RSS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RSS
+//
+//  Created by Karthik Personal on 2/3/19.
+//  Copyright © 2019 VKT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
