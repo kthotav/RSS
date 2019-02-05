f404a332d344724be451f9e2cd587d130bae29b3
Karthik Thota <kthotav@gmail.com>
2019-02-04 22:02:34 -0800

    RS1: Added News API Key

---
+++ RSS/RSS/Keys.swift
@@ -0,0 +1,11 @@
+//
+//  Keys.swift
+//  RSS
+//
+//  Created by Karthik Personal on 2/4/19.
+//  Copyright © 2019 VKT. All rights reserved.
+//
+
+import Foundation
+
+let NEWS_API_KEY = "565cf2e190b048ef93685fb1bc5ba114"
