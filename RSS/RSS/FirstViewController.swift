bc0fac483da19b61f1a7a8744c4e63d830f4f900
Karthik Thota <kthotav@gmail.com>
2019-02-13 23:26:11 -0800

    clean up

--- RSS/RSS/FirstViewController.swift
+++ RSS/RSS/FirstViewController.swift
@@ -13,24 +13,10 @@ class FirstViewController: UIViewController {
     // MARK: - View Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        loadSources()
-    }
-    
-    // MARK: - Private
-    private func loadSources() {
-        SourcesAPI.service.getSources { result in
-            switch result {
-            case .success(let sources):
-                print(sources)
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
 }
 
